f9d7c00b96bef1cb1d1a9627348f1c1f600f1a50
zee-ham-su <hsufiian@yahoo.com>
2025-02-23 08:15:58 +0000

    add API authentication URLs to the project

--- alx_travel_app/alx_travel_app/urls.py
+++ alx_travel_app/alx_travel_app/urls.py
@@ -39,5 +39,6 @@ urlpatterns = [
     path('', include('listings.urls')),
     path('swagger/', schema_view.with_ui('swagger',
          cache_timeout=0), name='schema-swagger-ui'),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 
 ]
